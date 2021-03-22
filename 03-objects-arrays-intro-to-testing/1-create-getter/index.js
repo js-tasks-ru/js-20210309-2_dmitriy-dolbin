48249986bf22e6f3c0e77d3b1535ede2547c981f
DoctorLecteur <roller97@yandex.ru>
2021-03-22 23:40:56 +0300

    done createGetter task

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,21 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const aKeys = path.split('.');
+  return function getter(obj) {
+    let nIndexKey = 0;
+    let bIsObj = false;
+    let objNew = obj;
+    while (!bIsObj) {
+      if (objNew[aKeys[nIndexKey]] === Object(objNew[aKeys[nIndexKey]])) {
+        bIsObj = false;
+        objNew = objNew[aKeys[nIndexKey]];
+        nIndexKey++;
+      } else {
+        bIsObj = true;
+        objNew = objNew[aKeys[nIndexKey]];
+      }
+    }
+    return objNew;
+  }
 }
