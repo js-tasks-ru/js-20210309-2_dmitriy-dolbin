763cd9d055c9588049c47f79e62d24d98c408f6f
DoctorLecteur <roller97@yandex.ru>
2021-03-23 00:00:55 +0300

    done invertObj task

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,12 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (obj === Object(obj)) {
+    const aKeys = Object.keys(obj);
+    const map = new Map();
+    for (const key of aKeys) {
+      map.set(obj[key], key);
+    }
+    return Object.fromEntries(map.entries());
+  }
 }
