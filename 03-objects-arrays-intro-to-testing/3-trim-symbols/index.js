0afa53fb94eb907dd1fb070151c9c58a5441a013
Doctor_Lector <roller97@yandex.ru>
2021-03-26 21:41:44 +0300

    done trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,19 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (string === '' || size === undefined) return string;
+  if (size === 0) return '';
+  const arr = Array.from(string);
+  const objSize = {};
+  let newStr = "";
+  arr.forEach(function (item, index, array) {
+    objSize[item] = objSize[item] + 1 || 1;
+    if (objSize[item] <= size) {
+      newStr += item;
+      if (item !== array[index - 1] && index > 0) {
+        objSize[array[index - 1]] = 0;
+      }
+    }
+  });
+  return newStr;
 }
